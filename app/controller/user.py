1dc9dff84be78050f3308917e1f17c426a8f2b47
浅田清文 <kiyofumi.a.0122@gmail.com>
2022-10-23 11:55:31 +0900

    modify

--- app/controller/user.py
+++ app/controller/user.py
@@ -3,15 +3,14 @@ from sqlalchemy.orm import Session
 from typing import List
 from database import get_db
 import models.user as userModel
-
-from return_types.user import \
-    User as UserSchema
+from res_schemas.user import User
 
 router = APIRouter()
 
+# /user
 # read
-@router.get('/', response_model=List[UserSchema])
+@router.get('/', response_model=List[User])
 async def read_users(db: Session = Depends(get_db)):
     data = userModel.read_users(db=db)
-    print("--------------data",data)
+    print("controller---------------",data)
     return data
